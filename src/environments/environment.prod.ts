c95c3c608c7d4280cb588de99b2d8128d956dbde
Monica <monica.garrido@accenture.com>
2023-03-23 12:11:18 +0100

    Update backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,8 +5,8 @@ export const environment = {
     clientId: '03e82b0d-a368-42a6-9d81-9a0391e8592d',
     discoveryEndpoint: 'https://us-south.appid.cloud.ibm.com/oauth/v4/ff2d5da9-4499-4ef0-9fc6-7e280c05ea9c/.well-known/openid-configuration'
   },
-  websocket: 'http://banking-1-alpha.apps.cluster-fkl5f.fkl5f.sandbox3089.opentlc.com',
-  backend: 'http://banking-1-alpha.apps.cluster-fkl5f.fkl5f.sandbox3089.opentlc.com'
+  websocket: 'http://banking-2-alpha.apps.cluster-fkl5f.fkl5f.sandbox3089.opentlc.com',
+  backend: 'http://banking-2-alpha.apps.cluster-fkl5f.fkl5f.sandbox3089.opentlc.com'
 };
 
 
