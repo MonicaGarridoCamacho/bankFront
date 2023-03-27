48e99c0ddbfd3e7e425d0bbe5db57624cd1bf0ef
Monica <monica.garrido@accenture.com>
2023-03-27 11:02:33 +0200

    Update backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,8 +9,8 @@ export const environment = {
     clientId: '03e82b0d-a368-42a6-9d81-9a0391e8592d',
     discoveryEndpoint: 'https://us-south.appid.cloud.ibm.com/oauth/v4/ff2d5da9-4499-4ef0-9fc6-7e280c05ea9c/.well-known/openid-configuration'
   },
-  websocket: 'http://172.30.231.238',
-  backend: 'http://172.30.231.238'
+  websocket: 'https://banking-1-alpha.apps.cluster-8hvnk.8hvnk.sandbox2233.opentlc.com ',
+  backend: 'https://banking-1-alpha.apps.cluster-8hvnk.8hvnk.sandbox2233.opentlc.com '
 };
 /*
  * For easier debugging in development mode, you can import the following file
